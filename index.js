c5a5542e38f309c6ac0e4ce5915e34017207c9e4
yvonnendungu <ndunguyvonne10@gmail.com>
2024-04-16 17:23:11 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  };
+  
+
+  let configObj = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+
+    function submitData(userName, userEmail) {
+   const config = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+          body: JSON.stringify({
+           name:  `${userName}`,
+                    email: `${userEmail}`
+          }),
+      };
+   
+return fetch('http://localhost:3000/users', config)
+.then(resp => resp.json())
+.then(handleData)
+.catch(handleErr)
+}
+function handleData(data) {
+document.body.innerHTML = data["id"]
+}
+
+function handleErr(err) {
+document.body.innerHTML += `${err.message}`
+}
